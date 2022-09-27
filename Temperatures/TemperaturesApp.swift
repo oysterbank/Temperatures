a4326178d924ac7a1c5d8670c0ce18f693f60071
Kris Laratta <krislaratta@gmail.com>
2022-09-27 08:07:43 -0400

    Initial Commit

---
+++ Temperatures/TemperaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperaturesApp.swift
+//  Temperatures
+//
+//  Created by Kris Laratta on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TemperaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
