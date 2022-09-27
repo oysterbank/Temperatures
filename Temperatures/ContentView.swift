0759a4875b2976d7d2877f2ad3db2d8b43234e30
Kris Laratta <krislaratta@gmail.com>
2022-09-27 12:28:32 -0400

    convert temperatures

--- Temperatures/ContentView.swift
+++ Temperatures/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var temperatureValue = 0.0
+    @State private var inputUnit = "Fahrenheit"
+    @State private var outputUnit = "Celsius"
+    let units = ["Fahrenheit", "Celsius", "Kelvin"]
+    
+    func convertTemp(temp: Double, from inputTempType: UnitTemperature, to outputTempType: UnitTemperature) -> Double {
+        let input = Measurement(value: temp, unit: inputTempType)
+        let output = input.converted(to: outputTempType)
+        return output.value
+    }
+    
+    var outputTemperature: Double {
+        // Convert to Kelvin first
+        var normalizedTemp = 0.0
+        if (inputUnit == "Fahrenheit") {
+            normalizedTemp = convertTemp(temp: temperatureValue, from: .fahrenheit, to: .kelvin)
+        } else if (inputUnit == "Celsius") {
+            normalizedTemp = convertTemp(temp: temperatureValue, from: .celsius, to: .kelvin)
+        } else {
+            normalizedTemp = temperatureValue
         }
-        .padding()
+        
+        // Then convert to output unit
+        if (outputUnit == "Fahrenheit") {
+            return round(convertTemp(temp: normalizedTemp, from: .kelvin, to: .fahrenheit) * 100) / 100
+        } else if (outputUnit == "Celsius") {
+            return round(convertTemp(temp: normalizedTemp, from: .kelvin, to: .celsius) * 100) / 100
+        } else {
+            return round(normalizedTemp * 100) / 100
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Input Temperature", value: $temperatureValue, format: .number)
+                        .keyboardType(.decimalPad)
+                } header: {
+                    Text("Input Temperature")
+                }
+                
+                Section {
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Input Unit")
+                }
+                
+                Section {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Output Unit")
+                }
+                
+                Section {
+                    Text(outputTemperature, format: .number)
+                } header: {
+                    Text("Output Temperature")
+                }
+            }
+            .navigationTitle("Temperatures")
+        }
     }
 }
 
